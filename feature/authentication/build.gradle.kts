a33e175b1b31fb03d812d2b7a8128a09eb045913
Terry Mochire <terrybmochire@gmail.com>
2023-06-15 15:04:32 +0300

    Add new dynamic feature.

---
+++ feature/authentication/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.dynamic-feature")
+    kotlin("android")
+}
+android {
+    namespace = "com.example.feature.authentication"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":app"))
+    implementation(libs.core.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.annotation)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Navigation"
 include(":app")
+include(":feature:authentication")
